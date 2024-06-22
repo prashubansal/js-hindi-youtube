fd276a2b448acb5dbd01f4f51f35181489fb7816
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-06-22 04:35:52 +0000

    datatypes comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1); //converted datatype to a number
+// console.log("02" > 1);
+// console.log("02" == 2);
+//sometimes result not as per expectations
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//equality check == and comparisions > < >= <= work differently
+//Comparisions convert null to a number, treating as a 0.  
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
+
+// Summary = avoid these types of comparisions
