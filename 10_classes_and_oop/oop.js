4765f3094696d786b30186805edf52f49c4e0bcc
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-01 12:39:02 +0000

    OOP intro and constructor function

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -5,11 +5,37 @@ const user = {
 
     getUserDetails: function(){
         // console.log("Got user detial from database");
-        console.log(`Username: ${username}`);
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
     }
 
 
 }
 
-console.log(user["username"])
-console.log(user.getUserDetails());
+
+// console.log(user["username"])
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
+
+// constructor function
+// Step1: creates an empty JS object using "new" operator -> something like this (const x = {})
+// Step2: Cons_func call hota hai because of "new operator" and using "this" we set various properties and methods in this new object(because this empty object is nothing but an instance of an this object)
+// Step3: Cons_func jitne bhi arguments hote hai unko newly created "empty object" ke andar pack karta hai (using "this") and ye cons_func aapko vo new object return kar deta hai. 
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+    return this
+}
+
+const userOne = new User("prashu", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+// console.log(userTwo);
