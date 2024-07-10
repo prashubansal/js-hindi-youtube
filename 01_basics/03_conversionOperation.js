1ea8bceec853aa4803b44225bb604a427c1df594
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-10 08:40:18 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -64,6 +64,8 @@ let gameCounter = 100
 newCounter = gameCounter++;
 console.log(gameCounter, newCounter);
 
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
