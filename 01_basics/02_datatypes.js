a2639532b7db476ed8188af077042152820c057a
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-10 06:21:03 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,26 +1,27 @@
-"use strict"; //treat all JS code as newer version
+"use strict"; // treat all JS code as a newer version
 
 // alert(3+3) // we are using nodejs, not browser
 
-console.log(3+3)
+// code readability should be high
+console.log(3+3); console.log("Hitesh") 
 
-console.log("prashu")
+let name = "hitesh"
+let age = 18
+let isLoggedIn = true
+let state;
 
-let name = "prashu"
-let age = 25
-let isLoggedIn = false
 
-// number => 2^53
-// bigint
+// number => 2 to power 53
+// bigint => 
 // string => ""
 // boolean => true/false
-// null => standalone value
-// undefined => 
+// null => standalone value (it means, null value assign hui hai)
+// undefined => abhi value assign nhi hui hai
 // symbol => unique
 
 
-// object 
 
+// object
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
