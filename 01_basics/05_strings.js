38d77e80545f8ad9202eacad6ed613150277b941
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-12 04:27:27 +0000

    strings interpolations and methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,13 @@
-const name = "prashu"
+const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " value");
+// console.log(name + repoCount + " Value");
 
+// String Interpolation 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('prashu-pb-com')
+const gameName = new String('hitesh-hc-com')
+// console.log(typeof(gameName));
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,27 +15,26 @@ const gameName = new String('prashu-pb-com')
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(4));
-// console.log(gameName.indexOf('h'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+const newString = gameName.substring(-8, 4)
+// console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+// console.log(anotherString);
 
-const newStringOne = "    hitesh   "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+const newStringOne = "    hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))
 
-console.log(url.includes('nwkjcj'))
+// console.log(url.includes('hitesh'))
 
-console.log(gameName.split('-'));
+// console.log(url.includes('sundar'))
 
-
-
-
+console.log(gameName.split('-'))
