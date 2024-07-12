e7e282802c0a46f28764908d4ee2c95c261cccd7
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-12 08:00:50 +0000

    conversion (datatypes ->  array) & concatenation of arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,32 +1,37 @@
-const marvel_heros = ["thor", "Ironman", "spiderman"]
+const marvel_heros = ["thor", "spiderman", "Ironman"]
 const dc_heros = ["superman", "flash", "batman"]
 
 // marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][1]); 
+// console.log(marvel_heros[3][1]);
 
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
-
+// This method is prefered over "conact".
+const all_new_heros = [...marvel_heros, ...dc_heros] 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
 
-console.log(real_another_array);
 
+// convert different datatypes into array
+// check if it is a array or not
+console.log(Array.isArray("Hitesh")) 
 
-console.log(Array.isArray("Hitesh"))
+//converts string into array
 console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"}))  // interesting
 
+// convert Object into 
+console.log(Array.from({name: "Hitesh"})) // Interesting
+ 
+// convert multiple variables/arrays into a single array
 let score1 = 100
 let score2 = 200
 let score3 = 300
-
-console.log(Array.of(score1,score2,score3));
+console.log(Array.of(score1, score2, score3));
 
