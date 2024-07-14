f1b314d32d74502dec90d0a0701c3d9f6228b65c
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-14 12:23:29 +0000

    merge objects & methods of object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,9 +1,9 @@
-// const tinderUser = new Object()
+// const tinderUser = new Object() // singleton object
 const tinderUser = {}
 
 tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
-tinderUser.isLoggedIn = false 
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
@@ -11,29 +11,31 @@ const regularUser = {
     email: "some@gmail.com",
     fullname: {
         userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
+            firstName: "hitesh",
+            lastName: "choudhary"
         }
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstName);
 
+
+// merge multiple Objects into a single object
 const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj4 = {5: "a", 6: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
 
-// const obj3 = {obj1, obj2}
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj4 = {obj1, obj2, obj3}
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+// Object.assign(target, source)
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+
+// best way to merge multiple Objects
+const obj4 = {...obj1, ...obj2, ...obj3}
+// console.log(obj4);
+
 
 const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
     {
         id: 1,
         email: "h@gmail.com"
@@ -49,37 +51,13 @@ const users = [
 ]
 
 users[1].email
-// console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// some methods of Objects
+console.log(tinderUser);
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-// ************************** Object De-structure **************************
-
-const course = {
-    coursename: "js in hindi",
-    price: "999",
-    courseInstructor: "hitesh"
-}
-
-// course.courseInstructor
-
-const {courseInstructor: instructor} = course
-
-// console.log(courseInstructor);
-console.log(instructor);
-
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
-
-[
-    {},
-    {},
-    {}
-]
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLogged'));
