97dd3cfd70f94a656e0809e98d06614917d73731
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-15 03:37:56 +0000

    IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,4 +1,6 @@
 // Immediately Invoked function Expressions (IIFE)
+// Global scope ke pollution se problem hoti hai kai baar, so to save our function from it we use "IIFE".
+// have to use ; for ending the context of IIFE.
 
 (function chai (){
     // named IIFE
