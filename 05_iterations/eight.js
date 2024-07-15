acac5ebbe5b1a471a768ccb4178d618e53d505f3
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-15 13:14:03 +0000

    reduce method

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,13 +1,13 @@
-// reduce method
+// "REDUCE" method of an array
 
-const myNums = [1, 2, 3]
+const myNums = [1,2,3]
 
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
 // }, 0)
 
-const myTotal = myNums.reduce((acc, currval) => acc+currval, 0)
+const myTotal = myNums.reduce((acc, currval) => acc + currval, 0)
 
 // console.log(myTotal);
 
@@ -21,15 +21,15 @@ const shoppingCart = [
         price: 999
     },
     {
-        itemName: "mobi dev course",
-        price: 5999
-    },
-    {
         itemName: "data science course",
         price: 12999
-    }
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    }   
 ]
 
-const Total = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
-console.log(Total);
+console.log(priceToPay);
