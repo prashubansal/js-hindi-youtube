8d69341a6e11560a6d512aa25274c04745f91947
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-15 13:13:47 +0000

    map method

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,21 +1,16 @@
-// map function
+// "MAP" method of an array
+// returns an array after doing some operations
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 
-// const newNums = myNums.map((num) => {return num + 10})
+// const newNums = myNumbers.map((num) => num + 10)
 
-// const newNums = []
 
-// myNums.forEach((num) => {
-//     if(true){
-//         num = num + 10
-//         newNums.push(num)
-//     }
-// })
-
-const newNums = myNums
-        .map((num) => num * 10 )
+// ++++++++++++++++++++ Chaining of methods +++++++++++++++++++++
+const newNums = myNumbers
+        .map((num) => num * 10)
         .map((num) => num + 1)
         .filter((num) => num >= 40)
 
 console.log(newNums);
+
