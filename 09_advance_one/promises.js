042b173e4fbd3eacd8dff8b5e6181a491563ecfe
prashubansal <56062861+prashubansal@users.noreply.github.com>
2024-07-20 04:39:44 +0000

    promise and fetch basics

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,43 +1,58 @@
+// Promise -> jo bhi aapne usko task diya hai vo abhi ke abhi queue mai lag ke complete nhi ho ga
+// queue mai toh lag gya hai, par abhi ke abhi usko load nhi kiya gya hai, abhi complete nhi ho sakta vo.
+
+// Promise -> The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+// Some tasks that does not execute instantly
+// File access
+// database ko request bheji hai
+// calculation can be complicated and time consuming
+// cryptography -> password encryption
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// How to create a promise
 const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
-    // DB calls, cryptography, network
+    // DB calls, cryptography, network calls
     setTimeout(function(){
         console.log('Async task is complete');
         resolve()
     }, 1000)
 })
 
+// Comnsumption of promise
 promiseOne.then(function(){
-    console.log("Promise consumed");
+    console.log("Promise consumed")
 })
 
 new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log("Async task 2");
+        console.log("Async task 2")
         resolve()
     }, 1000)
 }).then(function(){
-    console.log("Async 2 resolved");
+    console.log("Async 2 resolved")
 })
 
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
-        resolve({username: "chai", email: "chai@example.com"
+        resolve({username: "Chai", email: "chai@example.com"
         })
     }, 1000)
 })
 
-
 promiseThree.then(function(user){
-    console.log(user);
+    console.log(user)
 })
 
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true;
+        let error = true
         if(!error){
             resolve({username: "hitesh", password: "123"})
-        }else{
+        }
+        else{
             reject('ERROR: Something went wrong')
         }
     }, 1000)
@@ -47,58 +62,60 @@ promiseFour
 .then((user) => {
     console.log(user);
     return user.username
-}).then((username) => {
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
+})
+.then((username) => {
+    console.log(username)
+})
+.catch(function(error){
+    console.log(error)
+}).finally(() => {
+    console.log("The promise is either resolved or rejected")
+})
+
+
 
 const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true;
+        let error = true
         if(!error){
             resolve({username: "javascript", password: "123"})
-        }else{
+        }
+        else{
             reject('ERROR: JS went wrong')
         }
     }, 1000)
 })
 
 async function consumePromiseFive(){
-    try{
+    try {
         const response = await promiseFive
-        console.log(response);
+        console.log(response)
     } catch (error){
-        console.log(error);
+        console.log(error)
     }
 }
 
 consumePromiseFive()
 
-// async function getAllUsers (){
-// try {
-//     const respone = await fetch("https://jsonplaceholder.typicode.com/users")
-
-//     const data = await respone.json()
-//     console.log(data);
-// } catch (error) {
-//     console.log("E: ", error);
-// }
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data)
+//     }
+//     catch (error){
+//         console.log("E: ", error)
+//     }
 // }
 
 // getAllUsers()
 
-fetch('https://api.github.com/users/hiteshchoudhary')
+// fetch() -> returns promise datatype
+fetch("https://api.github.com/users/hiteshchoudhary")
 .then((response) => {
     return response.json()
 })
 .then((data) => {
-    console.log(data);
+    console.log(data)
 })
 .catch((error) => console.log(error))
-
-
-
-
-
-
